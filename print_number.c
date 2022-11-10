45f331e756873b1d68985ae29e44b413ded53165
Yonatan-Yaakov Koren <yonatankoren>
2022-11-10 16:28:23 +0200

    print correct number

--- print_number.c
+++ print_number.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int course_number[6] = {4, 2, 1, 4, 3, 2};
+int course_number[6] = {2, 3, 4, 1, 2, 4};
 int main() {
-	for(int i = 6; i > 0; i--) {
+	for(int i = 0; i < 6; i++) {
 		printf("%d", course_number[i]);
 	}
 	printf("\n");
